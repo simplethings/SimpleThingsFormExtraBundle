38522d0ba0435118444f6356ecd680b98d054445
jsor <jan@sorgalla.com>
2011-08-05 14:03:20 +0200

    Add fallback autoloader for standalone bundle testing

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -10,3 +10,12 @@ $loader->registerNamespaces(array(
     'Symfony' => $_SERVER['SYMFONY'],
 ));
 $loader->register();
+
+spl_autoload_register(function($class) {
+    if (0 === strpos($class, 'SimpleThings\\FormExtraBundle')) {
+        $file = dirname(__DIR__).str_replace('\\', DIRECTORY_SEPARATOR, substr($class, strlen('SimpleThings\\FormExtraBundle'))) . '.php';
+        if (file_exists($file)) {
+            require $file;
+        }
+    }
+}, true);
