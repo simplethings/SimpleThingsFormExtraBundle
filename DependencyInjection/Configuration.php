fc165904b4f9543f3169c9891ec096b28fa4f211
jsor <jan@sorgalla.com>
2011-08-08 12:09:45 +0200

    Add canBeUnset()

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -14,6 +14,7 @@ class Configuration implements ConfigurationInterface
         return $builder->root('simple_things_form_extra')
             ->children()
                 ->arrayNode('recaptcha')
+                    ->canBeUnset()
                     ->children()
                         ->scalarNode('public_key')->isRequired()->cannotBeEmpty()->end()
                         ->scalarNode('private_key')->isRequired()->cannotBeEmpty()->end()
