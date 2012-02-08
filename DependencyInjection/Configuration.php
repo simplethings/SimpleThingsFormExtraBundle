a9e17cff8caafae883a64baa1a69e0d58236bbc7
Benjamin Eberlei <kontakt@beberlei.de>
2012-02-09 00:49:04 +0100

    Add Test for Generator

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -1,4 +1,15 @@
 <?php
+/**
+ * SimpleThings FormExtraBundle
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to kontakt@beberlei.de so I can send you a copy immediately.
+ */
 
 namespace SimpleThings\FormExtraBundle\DependencyInjection;
 
