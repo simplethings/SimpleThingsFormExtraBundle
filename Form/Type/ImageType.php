cc51dacc2be89784ea6787d831ff81c56d19d0e2
Ivan Borzenkov <ivan1986@list.ru>
2013-06-18 11:33:11 +0400

    fix image type to 2.3 compatible

--- Form/Type/ImageType.php
+++ Form/Type/ImageType.php
@@ -54,20 +54,20 @@ class ImageType extends AbstractType
     {
         $data = $form->getData();
 
-        if ($data && (strpos($data->getPath(), $form->getAttribute('base_path')) === 0)) {
+        if ($data && (strpos($data->getPath(), $form->getConfig()->getAttribute('base_path')) === 0)) {
             /* @var $data SplFileInfo */
-            $uri = str_replace(realpath($form->getAttribute('base_path')), $form->getAttribute('base_uri'), $data->getRealPath());
+            $uri = str_replace(realpath($form->getConfig()->getAttribute('base_path')), $form->getConfig()->getAttribute('base_uri'), $data->getRealPath());
             if ('/' !== DIRECTORY_SEPARATOR) {
                 $uri = str_replace(DIRECTORY_SEPARATOR, '/', $uri);
             }
             $view->vars['image_uri'] = $uri;
-        } else if ($form->hasAttribute ('no_image_placeholder_uri') && $uri = $form->getAttribute ('no_image_placeholder_uri')) {
+        } else if ($form->getConfig()->hasAttribute ('no_image_placeholder_uri') && $uri = $form->getConfig()->getAttribute ('no_image_placeholder_uri')) {
             $view->setAttribute('image_uri', $uri);
         }
 
-        $view->vars['image_alt'] = $form->getAttribute('image_alt');
-        $view->vars['image_height'] = $form->getAttribute('image_height');
-        $view->vars['image_width'] = $form->getAttribute('image_width');
+        $view->vars['image_alt'] = $form->getConfig()->getAttribute('image_alt');
+        $view->vars['image_height'] = $form->getConfig()->getAttribute('image_height');
+        $view->vars['image_width'] = $form->getConfig()->getAttribute('image_width');
     }
 
     /**
